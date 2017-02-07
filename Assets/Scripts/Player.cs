d75d2950478369bff46b4e88077b265f082c0fe7
Emile <emile.app@gmail.com>
2017-02-07 14:48:26 +0200

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class Player : MonoBehaviour {
+
+	public float gravity = -3.0f;
+	public float speed = 0.2f;
+
+	private CharacterController controller;
+
+	private Vector3 direction = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputH = Input.GetAxisRaw("Horizontal");
+
+		direction = Vector3.right * inputH;
+
+		controller.Move(direction * speed + Vector3.up * gravity);
+	}
+}
