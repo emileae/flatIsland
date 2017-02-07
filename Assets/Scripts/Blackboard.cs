926897726af931499960e75246f86958a31bf938
Emile Esterhuizen <emile@Emiles-MacBook-Air.local>
2017-02-08 00:08:56 +0200

    started with NPC target queueing

--- Assets/Scripts/Blackboard.cs
+++ Assets/Scripts/Blackboard.cs
@@ -7,6 +7,9 @@ public class Blackboard : MonoBehaviour {
 	public List<Transform> npcs = new List<Transform> ();
 	public List<NPC> npcScripts = new List<NPC> ();
 
+	private bool callingQueuedTargets = false;
+	public List<GameObject> waitingNPCTargets = new List<GameObject> ();
+
 	// Use this for initialization
 	void Start () {
 		GameObject npcContainer = GameObject.Find ("NPCs");
@@ -19,21 +22,28 @@ public class Blackboard : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (waitingNPCTargets.Count > 0 && !callingQueuedTargets) {
+			callingQueuedTargets = true;
+			StartCoroutine (CallQueuedTargets());
+		}
 	}
 
 	public void CallNearestNPC(GameObject target){
-		bool gotInitialDistance = false;
+
+		// TODO
+		// queue NPC calls when there are no available NPCs.....
+
+		bool gotInitialIndex = false;
 		float minDistance = 0;
 		int minIndex = 0;
 		for (int i = 0; i < npcScripts.Count; i++) {
 			Debug.Log ("NPC " + i + " busy: " + npcScripts [i].busy);
 			if (!npcScripts [i].busy) {
 				float distance = (npcs [i].position - target.transform.position).sqrMagnitude;
-				if (!gotInitialDistance) {
+				if (!gotInitialIndex) {
 					minDistance = distance;
 					minIndex = i;
-					gotInitialDistance = true;
+					gotInitialIndex = true;
 				}
 
 				if (distance < minDistance) {
@@ -43,10 +53,25 @@ public class Blackboard : MonoBehaviour {
 			}
 		}
 
-		// now send NPC to target
-		NPC closestNPC = npcScripts[minIndex];
-		closestNPC.target = target;
-		closestNPC.goToDestination = true;
+		// no NPCs found... place in queue
+		if (!gotInitialIndex) {
+			Debug.Log ("NO NPCSSSSSZZZZZ");
+			waitingNPCTargets.Add (target);
+		} else {
+			// now send NPC to target
+			NPC closestNPC = npcScripts [minIndex];
+			closestNPC.target = target;
+			closestNPC.goToDestination = true;
 
+			// remove from list of queued targets if its in the list
+			waitingNPCTargets.Remove (target);
+		}
+
+	}
+
+	IEnumerator CallQueuedTargets(){
+		yield return new WaitForSeconds (5.0f);
+		Debug.Log ("Called a Queued target......");
+		CallNearestNPC (waitingNPCTargets [0]);
 	}
 }
